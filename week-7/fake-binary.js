9097e561a2cbf105b05674ca75a27ae2e1fba357
command-gab <smallwoodgabrielle@gmail.com>
2021-10-30 15:07:46 -0400

    Fake Binary

---
+++ week-7/fake-binary.js
@@ -0,0 +1,15 @@
+// FAKE BINARY
+function fakeBin(x){
+    let strArray = x.split('')
+    let numArray = strArray.map(item => Number(item))
+    let binary = []
+    
+    for(let i = 0; i < numArray.length; i++) {
+      if(numArray[i] < 5) {
+        binary.push('0')
+      }else if(numArray[i] >= 5) {
+        binary.push('1')
+      }
+    }
+    return binary.join('')
+  }
