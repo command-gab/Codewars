a09857773d8b16cc595b3386eb6547c66dfe25bb
Gabrielle <88905557+command-gab@users.noreply.github.com>
2021-11-11 14:13:17 -0500

    Update will-you-make-it.js

--- week-7/will-you-make-it.js
+++ week-7/will-you-make-it.js
@@ -1,9 +1,5 @@
 // WILL YOU MAKE IT?
 const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
-    //P: takes in distanceToPump, mpg and fuelLeft | 
-    //R: return true if possible, false if not.
-    //E:
-    //P: if/else conditional, if distance (50) is less or equal to mpg (2) * fuelLeft (25)
       if(distanceToPump <= mpg * fuelLeft) {
         return true
       }else {
