981ab0a24493f67f310eb21a68c8eef69568890b
Gabrielle <88905557+command-gab@users.noreply.github.com>
2021-11-22 11:03:28 -0500

    Sum of Array Singles

--- week-7/sum-array-singles.js
+++ week-7/sum-array-singles.js
@@ -2,3 +2,5 @@
 function repeats(arr){
     return arr.filter( n => arr.indexOf(n) === arr.lastIndexOf(n) ).reduce((a, c) => a + c)
     };
+
+// https://www.codewars.com/kata/59f11118a5e129e591000134
